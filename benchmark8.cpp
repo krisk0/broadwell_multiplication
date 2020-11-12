938e6347445241eb41c33422beee7bf405f18d9e
krisk0 <me@localhost.com>
2020-11-12 23:06:56 +0300

    benchmark8: can also work for SIZE=4

--- benchmark8.cpp
+++ benchmark8.cpp
@@ -8,13 +8,19 @@ void __gmpn_mul_basecase(mp_ptr, mp_srcptr up, mp_size_t, mp_srcptr, mp_size_t);
 
 #include "automagic/mul8_store_once.h"
 
-uint64_t g_scratch[8];
-
-#define BASECASE(x, y, z) __gmpn_mul_basecase(x, y, 8, z, 8)
-#define MUL8(x, y, z) __gmpn_mul(x, y, 8, z, 8)
+#ifndef SIZE
+    #define SIZE 8
+#endif
+#define BASECASE(x, y, z) __gmpn_mul_basecase(x, y, SIZE, z, SIZE)
+#define MUL8(x, y, z) __gmpn_mul(x, y, SIZE, z, SIZE)
 #define CUSTOM(x, y, z) mul8_broadwell_store_once_wr(x, y, z)
-#define SIZE 8
-#define VOLUME (1000*1000*100)
+#ifndef VOLUME
+    #if SIZE == 4
+        #define VOLUME (1000*1000*150)
+    #else
+        #define VOLUME (1000*1000*100)
+    #endif
+#endif
 #define HELLO
 #define GOODBYE
 
