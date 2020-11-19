8c6f8da0c1b1925cd1cccbc0de8f9bace2732072
krisk0 <me@localhost.com>
2020-11-19 13:19:17 +0300

    configure: can expand %

--- configure.py
+++ configure.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import os, sys, re
 
 if (len(sys.argv) == 2) and (sys.argv[1] == 'clean'):
@@ -68,7 +70,7 @@ def expand_ampersand(src):
         target.h: srcA.h srcB.h ...
     into
         build $o/target.h: catenate $o/srcA.h $o/srcB.h ...
-    
+
     add $o/ before smth.exe
     '''
     src = directory_before_exe(src)
@@ -127,23 +129,52 @@ def option_in_brackets(src):
     result = '%s = $%s %s' % (m.group(2), m.group(2), m.group(3))
     return m.group(1) + '\n    ' + result + '\n'
 
-def expand_line(src):
+def escape_1dot(x):
+    for y in '.$':
+        x = x.replace(y, '\\' + y)
+    return x
+
+g_expand_percent_pattern = re.compile('build (.+?):(.+)')
+def expand_percent(src, ff):
+    '''
+    expand percent in s0%s1: s2%s3, consulting list of files ff
+    src may have \n inside
+    '''
+    src_merged = src.replace('\n', 'ы')
+    m = g_expand_percent_pattern.match(src_merged)
+    if not m:
+        return src
+    p = m.group(1).split('%')
+    if len(p) != 2:
+        return src
+    r = m.group(2)
+    tgt = []
+    pa = re.compile(escape_1dot(p[0]) + '(.+)' + escape_1dot(p[1]))
+    for f in ff:
+        m = pa.match(f)
+        if not m:
+            continue
+        tgt.append('build ' + f + ':' + r.replace('%', m.group(1)))
+    return '\n'.join(tgt).replace('ы', '\n')
+
+def expand_line(src, ff):
     if src[0] == '#':
         return src + '\n'
     tgt = exe_rule(src.rstrip())
     tgt = option_in_brackets(tgt)
+    tgt = expand_percent(tgt, ff)
     if tgt.find('\n') != -1:
-        # expand 1st line, send 2nd line as is
-        tgt = tgt.split('\n')
-        e0 = '\n'.join(expand_ampersand(tgt[0]))
-        tgt = e0 + '\n' + tgt[1]
+        tgt,rez = tgt.split('\n'),[]
+        for i in tgt:
+            rez += expand_ampersand(i)
+        tgt = '\n'.join(rez)
     else:
         tgt = '\n'.join(expand_ampersand(tgt))
     if (not tgt) or (tgt[-1] != '\n'):
         tgt += '\n'
     return tgt
 
-def do_it(o, i):
+def do_it(o, i, tt):
     for k,v in g_opt.items():
         o.write('%s = %s\n' % (k, v))
     o.write('\n')
@@ -163,10 +194,10 @@ def do_it(o, i):
             continue
         if prev:
             # ignore leading spaces in continuation
-            k = expand_line(prev + j.lstrip())
+            k = expand_line(prev + j.lstrip(), tt)
             prev = None
         else:
-            k = expand_line(j)
+            k = expand_line(j, tt)
         o.write(k)
 
     if g_s_to_o:
@@ -194,6 +225,59 @@ g_int_h = ' '.join(g_int_h)
 g_int_h0 = escape_dot(g_int_h)
 g_int_h0_plain = g_int_h0.replace('\\', '')
 
+def contains_any_of(x, y):
+    return 1 in [c in x for c in y]
+
+def list_of_targets_subr_1(rez, src):
+    src = directory_before_exe(src).split(' ')
+    for i in src:
+        if (not i) or contains_any_of(i, '[]%=@*:\'`'):
+            continue
+        if (i.find('/') == -1) and (not os.path.isfile(i)):
+            i = '$o/' + i
+        rez.add(i)
+
+g_colon_pattern = re.compile('(.+?):(.+)')
+def list_of_targets_subr(rez, src):
+    m = g_colon_pattern.match(src)
+    if not m:
+        return
+    list_of_targets_subr_1(rez, m.group(2))
+    m = m.group(1)
+    if m.find(' ') != -1:
+        list_of_targets_subr_1(rez, m)
+
+def list_of_targets(i):
+    '''
+    merge rows ending at $
+    prepend $o before smth.exe
+    return list of all files found after :
+    '''
+    tgt,prev = set(),None
+    # TODO: get rid of repeated code by using custom iterator
+    for j in i:
+        if len(j) < 2:
+            continue
+        if j[-2] == '$':
+            now = j[:-2]
+            if prev is None:
+                prev = now
+            else:
+                # ignore leading spaces in continuation
+                prev += now.lstrip()
+            continue
+        if prev:
+            # ignore leading spaces in continuation
+            list_of_targets_subr(tgt, prev + j.lstrip())
+            prev = None
+        else:
+            list_of_targets_subr(tgt, j)
+    tgt.remove('$o/phony')
+    return sorted(list(tgt))
+
 g_s_to_o = set()
+with open('build.ninja.in', 'rb') as g_i:
+    g_t = list_of_targets(g_i)
+
 with open('build.ninja.in', 'rb') as g_i, open('build.ninja', 'wb') as g_o:
-    do_it(g_o, g_i)
+    do_it(g_o, g_i, g_t)
