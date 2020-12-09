abdb2145d0e6b1e7200d9fe02cd6cc13091f94fb
krisk0 <me@localhost.com>
2020-12-09 10:14:29 +0300

    gen_test_toom22_broadwell_t: test toom22...<> for N=10 and 11

--- gen_test_toom22_broadwell_t.py
+++ gen_test_toom22_broadwell_t.py
@@ -1,5 +1,6 @@
 """
-test subroutine toom22_broadwell_t<N>() for a range of N
+test subroutines toom22_1x_broadwell_t<N> and toom22_2x_broadwell_t<N>() for a
+ range of N
 
 TODO: instead of generating code with repeated patterns, use loop over constexpr
 """
@@ -11,8 +12,9 @@ g_head = r'''
 
 #include "test-internal.h"
 
-#define MAX_N @
-constexpr auto SCRATCH_SIZE = itch::toom22_whatever_t<12, MAX_N>();
+#define MIN_N @min
+#define MAX_N @max
+constexpr auto SCRATCH_SIZE = itch::toom22_whatever_t<MIN_N, MAX_N>();
 #define RAND_SEED 20190725
 
 INT* g_scratch;
@@ -23,16 +25,26 @@ INT g_result_baad[2 * MAX_N];
 INT g_page_mask;
 INT g_page_unmask;
 
-#define NS(x)                                                                        \
-    namespace _ ## x {                                                               \
-    constexpr uint16_t SIZE = x;                                                     \
-                                                                                     \
-    void call_good() {                                                               \
-        mpn_mul_n(g_result_good + 0, g_a + 0, g_b + 0, SIZE);                        \
-    }                                                                                \
-                                                                                     \
-    void call_baad() {                                                               \
-        toom22_broadwell_t<SIZE>(g_result_baad + 0, g_scratch, g_a + 0, g_b + 0);    \
+template<uint16_t N>
+void
+call_toom22(mp_ptr rp, mp_ptr scr, mp_srcptr ap, mp_srcptr bp) {
+    if constexpr(N & 1) {
+        toom22_1x_broadwell_t<N>(rp, scr, ap, bp);
+    } else {
+        toom22_2x_broadwell_t<N>(rp, scr, ap, bp);
+    }
+}
+
+#define NS(x)                                                                       \
+    namespace _ ## x {                                                              \
+    constexpr uint16_t SIZE = x;                                                    \
+                                                                                    \
+    void call_good() {                                                              \
+        mpn_mul_n(g_result_good + 0, g_a + 0, g_b + 0, SIZE);                       \
+    }                                                                               \
+                                                                                    \
+    void call_baad() {                                                              \
+        call_toom22<SIZE>(g_result_baad + 0, g_scratch, g_a + 0, g_b + 0);          \
     }
 '''
 
@@ -57,13 +69,13 @@ main() {
 
 import os, sys
 
-g_min_n = 12
+g_min_n = int(os.getenv('min_n', 10))
 g_max_n = int(os.getenv('max_n', 51))
 
 def do_it(o):
     global g_max_n
     o.write(g_head.lstrip().replace('@me', os.path.basename(sys.argv[0])).\
-        replace('@', str(g_max_n)))
+        replace('@max', str(g_max_n)).replace('@min', str(g_min_n)))
     g_max_n += 1
     for i in range(g_min_n, g_max_n):
         o.write(g_piece % i)
